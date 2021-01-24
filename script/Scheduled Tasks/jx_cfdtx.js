3768a394e826da2766af3e7e1f8051de32a260bb
cjf935257177 <52241107+cjf935257177@users.noreply.github.com>
2021-01-25 04:50:27 +0800

    Update jx_cfdtx.js

--- script/Scheduled Tasks/jx_cfdtx.js
+++ script/Scheduled Tasks/jx_cfdtx.js
@@ -65,7 +65,7 @@ $.userName = '';
   if (!getTokens()) return;
   for (let i = 0; i < $.cookieArr.length; i++) {
     $.currentCookie = $.cookieArr[i];
-    $.currentToken = $.tokenArr[i];
+    $.currentToken = $.tokenArr[0];
     if ($.currentCookie) {
       $.userName =  decodeURIComponent($.currentCookie.match(/pt_pin=(.+?);/) && $.currentCookie.match(/pt_pin=(.+?);/)[1]);
       $.log(`\n开始【京东账号${i + 1}】${$.userName}`);
